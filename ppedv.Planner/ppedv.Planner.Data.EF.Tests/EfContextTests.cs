48b6664fe7b1608338682fa12c43fdd9313c9967
André <andrer@ppedv.de>
2019-10-31 11:35:25 +0100

    EF + DB + Tests

--- ppedv.Planner/ppedv.Planner.Data.EF.Tests/EfContextTests.cs
+++ ppedv.Planner/ppedv.Planner.Data.EF.Tests/EfContextTests.cs
@@ -1,4 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ppedv.Planner.Model;
+using System;
 
 namespace ppedv.Planner.Data.EF.Tests
 {
@@ -16,6 +18,49 @@ namespace ppedv.Planner.Data.EF.Tests
             Assert.IsFalse(context.Database.Exists());
             context.Database.Create();
             Assert.IsTrue(context.Database.Exists());
+        }
+
+        [TestMethod]
+        public void EfContext_can_CRUD_Mitarbeiter()
+        {
+            var m = new Mitarbeiter() { Name = $"Fred_{Guid.NewGuid()}", PersonalNummer = "P001" };
+
+            //CREATE
+            using (var con = new EfContext())
+            {
+                con.Mitarbeiter.Add(m);
+                con.SaveChanges();
+            }
+
+            //READ
+            using (var con = new EfContext())
+            {
+                var loaded = con.Mitarbeiter.Find(m.Id);
+                Assert.IsNotNull(loaded);
+                Assert.AreEqual(m.Name, loaded.Name);
+
+                //UPDATE
+                loaded.PersonalNummer = "X001";
+                con.SaveChanges();
+            }
+
+            //check UPDATE
+            using (var con = new EfContext())
+            {
+                var loaded = con.Mitarbeiter.Find(m.Id);
+                Assert.AreEqual("X001", loaded.PersonalNummer);
+
+                //DELETE
+                con.Mitarbeiter.Remove(loaded);
+                con.SaveChanges();
+            }
+
+            //check DELETE
+            using (var con = new EfContext())
+            {
+                var loaded = con.Mitarbeiter.Find(m.Id);
+                Assert.IsNull(loaded);
+            }
         }
     }
 }
