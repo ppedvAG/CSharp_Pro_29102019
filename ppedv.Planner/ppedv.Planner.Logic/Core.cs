9d8814c511dbb5e29f4556f95317b44615ac1b34
André <andrer@ppedv.de>
2019-10-31 14:07:22 +0100

    Best GUI ever

--- ppedv.Planner/ppedv.Planner.Logic/Core.cs
+++ ppedv.Planner/ppedv.Planner.Logic/Core.cs
@@ -1,13 +1,30 @@
-﻿using ppedv.Planner.Data.EF;
+﻿using ppedv.Planner.Model;
+using ppedv.Planner.Model.Contracts;
 using System;
 
 namespace ppedv.Planner.Logic
 {
     public class Core
     {
-        public Core()
-        {
+        public IRepository Repository { get; }
 
+        public Core(IRepository repo)
+        {
+            this.Repository = repo;
         }
+
+        public Core() : this(new Data.EF.EfRepository())
+        { }
+
+        public int CountTage(Urlaub urlaub)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Mitarbeiter GetMitarbeiterMitDenMeistenUrlaubstagen()
+        { 
+            throw new NotImplementedException();
+        }
+
     }
 }
---
+++ ppedv.Planner/ppedv.Planner.UI.DevConsole#/Program.cs
@@ -0,0 +1,24 @@
+﻿using ppedv.Planner.Logic;
+using ppedv.Planner.Model;
+using System;
+
+namespace ppedv.Planner.UI.DevConsole_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var core = new Core();
+
+            foreach (var m in core.Repository.GetAll<Mitarbeiter>())
+            {
+                Console.WriteLine($"{m.Name} {m.PersonalNummer}");
+            }
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
